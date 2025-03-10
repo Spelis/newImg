985388445aa36304b6c7b845a80edcb55003736d
Elis Eriksson <Spelis@duck.com>
2025-03-10 15:48:08 +0100

    Removed Windows only stuffs so Linux can work too :)

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # newImg / nimg
-Create new images right from your terminal,
-## How to use it?
+Multi-platform solution to create new images right from your terminal.
+Officially supports Windows and Linux
+# How to use it?
+## Windows
 ### From File Explorer
 * set nimg as the default program for .nimg files
 * create a new file using this format: `filename.WIDTHxHEIGHT.extension.nimg`
@@ -8,6 +10,20 @@ Create new images right from your terminal,
 ### From the Terminal
 * add the nimg executable to PATH (not required but recommended)
 * run `nimg filename.WIDTHxHEIGHT.extension` (you dont need the .nimg here)
+## Linux
+### Adding to PATH (using nimg from the terminal)
+Move `nimg` to a folder you remember, like `/opt/user/`
+
+Then in your .bashrc or .zshrc (or other) add a similar line to:
+
+`export PATH="/opt/user/:PATH"`
+
+This will export the directory to PATH. (Make sure the location exists, also user can be anything)
+
+---
+
+Now, just run `nimg filename.WIDTHxHEIGHT.extension`
+
 ## How to set as default?
 There are probably several ways to do it, but I use this one:
 * create a .nimg file. doesn't matter what it's called or what it contains, just create it.
--- main.cpp
+++ main.cpp
@@ -1,93 +1,79 @@
 #include <iostream>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "include/stb_image_write.h"
-#include <sstream>
-#include <vector>
-#include <string>
 #include <filesystem>
-#include <conio.h>
-#include <chrono>
+#include <string>
+#include <vector>
 
-std::vector<std::string> split_string(const std::string &str, const std::string &delim)
-{
-    std::vector<std::string> tokens;
-    size_t prev = 0, pos = 0;
-    do
-    {
-        pos = str.find(delim, prev);
-        if (pos == std::string::npos)
-            pos = str.length();
-        std::string token = str.substr(prev, pos - prev);
-        if (!token.empty())
-            tokens.push_back(token);
-        prev = pos + delim.length();
-    } while (pos < str.length());
-    return tokens;
+std::vector<std::string> split_string(const std::string &str,
+                                      const std::string &delim) {
+  std::vector<std::string> tokens;
+  size_t prev = 0, pos = 0;
+  do {
+    pos = str.find(delim, prev);
+    if (pos == std::string::npos)
+      pos = str.length();
+    std::string token = str.substr(prev, pos - prev);
+    if (!token.empty())
+      tokens.push_back(token);
+    prev = pos + delim.length();
+  } while (pos < str.length());
+  return tokens;
 }
 
-std::string basename(std::string filename)
-{
-    return filename.substr(filename.find_last_of("/\\") + 1);
+std::string basename(std::string filename) {
+  return filename.substr(filename.find_last_of("/\\") + 1);
 }
 
 namespace fs = std::filesystem;
 
 int fail(int code = 10) {
-    std::cout << "Error " << std::to_string(code) <<" Occured... Press Enter to exit...";
-    getch();
-    return code;
+  std::cout << "Error " << std::to_string(code)
+            << " Occured... Press Enter to exit...";
+  return code;
 }
 
-int main(int argc, char *argv[])
-{
-    try
-    {
-        srand(time(NULL));
-        if (argc != 2)
-        {
-            std::cerr << "[ERROR]: Too few or too many arguments: " << std::to_string(argc) << std::endl;
-            return fail(1);
-        }
-
-        std::vector<std::string> file = split_string(basename(argv[1]), ".");
-        std::vector<std::string> szs = split_string(file.at(1), "x");
-        std::vector<int> sz = {atoi(szs.at(0).c_str()), atoi(szs.at(1).c_str())};
-
-        std::vector<unsigned char> rgbaData(sz.at(0) * sz.at(1) * 4); // Ensure correct size
-        int index = 0;
-
-        for (int i = 0; i < sz.at(0) * sz.at(1); i++)
-        {
-            rgbaData[index++] = rand()%255; // R
-            rgbaData[index++] = rand()%255; // G
-            rgbaData[index++] = rand()%255; // B
-            rgbaData[index++] = 255;       // A
-        }
-
-        try
-        {
-            fs::remove(argv[1]);
-        }
-        catch (const fs::filesystem_error &e)
-        {
-            std::cerr << "[ERROR]: Error deleting file: " << e.what() << std::endl;
-            fail(2);
-        }
-
-        fs::path finalpath = (file.at(0) + "." + file.at(2));
-        // Save the image as PNG
-        if (!stbi_write_png(finalpath.string().c_str(),
-                            sz.at(0), sz.at(1), 4, rgbaData.data(),
-                            sz.at(0) * 4))
-        {
-            return fail(3);
-        }
-
-        return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+  try {
+    srand(time(NULL));
+    if (argc != 2) {
+      std::cerr << "[ERROR]: Too few or too many arguments: "
+                << std::to_string(argc) << std::endl;
+      return fail(1);
     }
-    catch (const std::exception &e)
-    {
-        std::cerr << "[ERROR]: " << e.what() << std::endl;
-        return fail(4);
+
+    std::vector<std::string> file = split_string(basename(argv[1]), ".");
+    std::vector<std::string> szs = split_string(file.at(1), "x");
+    std::vector<int> sz = {atoi(szs.at(0).c_str()), atoi(szs.at(1).c_str())};
+
+    std::vector<unsigned char> rgbaData(sz.at(0) * sz.at(1) *
+                                        4); // Ensure correct size
+    int index = 0;
+
+    for (int i = 0; i < sz.at(0) * sz.at(1); i++) {
+      rgbaData[index++] = rand() % 255; // R
+      rgbaData[index++] = rand() % 255; // G
+      rgbaData[index++] = rand() % 255; // B
+      rgbaData[index++] = 255;          // A
     }
+
+    try {
+      fs::remove(argv[1]);
+    } catch (const fs::filesystem_error &e) {
+      std::cerr << "[ERROR]: Error deleting file: " << e.what() << std::endl;
+      fail(2);
+    }
+
+    fs::path finalpath = (file.at(0) + "." + file.at(2));
+    // Save the image as PNG
+    if (!stbi_write_png(finalpath.string().c_str(), sz.at(0), sz.at(1), 4,
+                        rgbaData.data(), sz.at(0) * 4)) {
+      return fail(3);
+    }
+
+    return EXIT_SUCCESS;
+  } catch (const std::exception &e) {
+    std::cerr << "[ERROR]: " << e.what() << std::endl;
+    return fail(4);
+  }
 }
